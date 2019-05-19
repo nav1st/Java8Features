4d18fd7acb411bf0704c356f999052e1fcc504e5
nav1st <nav1st@gmail.com>
2019-05-20 02:34:19 +0530

    Update ConvertListToMap.java

--- src/ConvertListToMap.java
+++ src/ConvertListToMap.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.ArrayList;import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -7,7 +6,7 @@ import java.util.stream.Collectors;
 public class ConvertListToMap {
 	public static void main(String[] args) {
 		Employee emp = new Employee(1,"Abhi",35,"m");
-		Employee emp1 = new Employee(2,"nootri",34,"f");
+		Employee emp1 = new Employee(2,"Ava",34,"f");
 		
 		List<Employee> emps = new ArrayList<>();
 		emps.add(emp);
